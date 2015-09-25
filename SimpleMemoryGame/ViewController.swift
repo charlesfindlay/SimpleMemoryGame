a23c93b01d7b93598aa18507a70db6bdb3f249b1
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-09-25 09:03:36 -0400

    Fully working core game functions

--- SimpleMemoryGame/ViewController.swift
+++ SimpleMemoryGame/ViewController.swift
@@ -22,6 +22,11 @@ class ViewController: UIViewController {
     var count = 0
     var guess1 = ""
     var guess2 = ""
+    var guess1Button: UIButton?
+    var guess2Button: UIButton?
+    var timer = NSTimer()
+
+    
     
     var tiles = [String: String]()
     
@@ -55,39 +60,69 @@ class ViewController: UIViewController {
     }
     
     
-    func flipTile(chosenTile: String) {
+    func flipTile(chosenTile: String, sender: UIButton) {
         count++
+        sender.enabled = false
         let choiceValue = tiles[chosenTile]!
         print(choiceValue)
         if count == 1 {
             guess1 = choiceValue
-            print("Guess 1 = \(guess1)")
+            //guess1Button = sender
+            sender.setTitle(choiceValue, forState: .Normal)
+            guess1Button = sender
+            
         } else {
             count = 0
             guess2 = choiceValue
-            print("Guess 2 = \(guess2)")
-            checkForMatch()
+            guess2Button = sender
+            sender.setTitle(choiceValue, forState: .Normal)
+            timer = NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: Selector("checkForMatch"), userInfo: nil, repeats: false)
+            
         }
     }
     
     func checkForMatch() {
+        timer.invalidate()
         print("Your guesses are \(guess1) and \(guess2)")
         if guess1 == guess2 {
             print("You found a match")
+            guess1Button?.hidden = true
+            guess2Button?.hidden = true
+            
         } else {
             print("No match")
+            guess1Button?.setTitle("?", forState: .Normal)
+            guess2Button?.setTitle("?", forState: .Normal)
+            guess1Button?.enabled = true
+            guess2Button?.enabled = true
         }
     }
     
     
     
-    @IBAction func pressedTile1(sender: AnyObject) {
-        flipTile("tile1")
-        sender.setTitle(tiles["tile1"], forState: .Normal)
+    @IBAction func pressedTile1(sender: UIButton) {
+        flipTile("tile1", sender: sender)
     }
     
+    @IBAction func pressedTile2(sender: UIButton) {
+        flipTile("tile2", sender: sender)
+    }
     
+    @IBAction func pressedTile3(sender: UIButton) {
+        flipTile("tile3", sender: sender)
+    }
     
+    @IBAction func pressedTile4(sender: UIButton) {
+        flipTile("tile4", sender: sender)
+    }
+    
+    @IBAction func pressedTile5(sender: UIButton) {
+        flipTile("tile5", sender: sender)
+    }
+    
+    @IBAction func pressedTile6(sender: UIButton) {
+        flipTile("tile16", sender: sender)
+    }
     
 
 }
